5cedeb932197eff938e67f1d2763b5f28ce11fc6
kal-el <74044780+PonganeshP@users.noreply.github.com>
2023-01-11 22:27:32 +0530

    checking props children

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ class App extends Component {
       date: new Date(),
       name: "Jake Sully",
       login: true,
+      people: ["Jake Sully", "Neytri", "Neteyam", " Lo'ak"],
     };
   }
   componentDidMount() {
-    const timer = () =>
-      this.setState({
-        date: new Date(),
-      });
-    setInterval(timer, 1000);
+    // const timer = () =>
+    //   this.setState({
+    //     date: new Date(),
+    //   });
+    // setInterval(timer, 5000);
   }
   render() {
     return (
@@ -24,10 +25,20 @@ class App extends Component {
         <h1>Welcome to Pandora</h1>
         <p>{this.state.date.toLocaleString()}</p>
         {this.state.login ? (
-          <Hello name={this.state.name} />
+          <Hello name={this.state.name}>
+            <h1>Varum aana varadhuu.....</h1>
+            <h2>Hello mister...</h2>
+          </Hello>
         ) : (
           <h2>Please Login</h2>
         )}
+
+        <h3>People in Pandoraa</h3>
+        <div>
+          {this.state.people.map((p) => {
+            return <p key={p.toString()}>{p}</p>;
+          })}
+        </div>
       </div>
     );
   }
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Hello(props) {
-  return <h2>Hello {props.name}</h2>;
+  return (
+    <div>
+      <h2>Hello {props.name}</h2>
+      {props.children}
+      {console.log(props.children[0].props)}
+    </div>
+  );
 }
 
 export default Hello;
